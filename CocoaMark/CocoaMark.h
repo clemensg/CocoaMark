de00d13e797ed64a0f6eda081f49b6c2b0faf628
Clemens Gruber <clemensgru@gmail.com>
2015-03-14 15:04:34 +0100

    Initial Commit

---
+++ CocoaMark/CocoaMark.h
@@ -0,0 +1,13 @@
+//
+//  CocoaMark.h
+//  CocoaMark
+//
+//  Created by Clemens Gruber on 14.03.15.
+//  Copyright (c) 2015 Gruber Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CocoaMark : NSObject
+
+@end
---
+++ CocoaMark/CocoaMark.m
@@ -0,0 +1,13 @@
+//
+//  CocoaMark.m
+//  CocoaMark
+//
+//  Created by Clemens Gruber on 14.03.15.
+//  Copyright (c) 2015 Gruber Software. All rights reserved.
+//
+
+#import "CocoaMark.h"
+
+@implementation CocoaMark
+
+@end
